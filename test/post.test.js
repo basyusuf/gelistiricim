686317e804c068fc821e121cd4f8a205a283612e
basyusuf <y.penava@hotmail.com>
2020-04-05 15:39:09 +0300

    Post test

---
+++ test/post.test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+let token;
+
+describe('/api/users Tests',()=>{
+    before((done)=>{
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                userName:'test',
+                password:'test123'
+            })
+            .end((err,res)=>{
+                token = res.body.token;
+                done();
+            });
+    });
+
+    describe('/GET Post',()=>{
+        it('it should GET all the post',(done)=>{
+            chai.request(server)
+                .get('/api/post')
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+});
