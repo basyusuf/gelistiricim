a661b49484dd940cdbe46b11220e302b3caa789e
basyusuf <y.penava@hotmail.com>
2020-04-13 05:36:55 +0300

    Duplicate error handler

--- middleware/customErrorHandler.js
+++ middleware/customErrorHandler.js
@@ -7,6 +7,10 @@ const customErrorHandler = (err,req,res,next)=>{
     if(err.name === "ValidationError"){
         customError = new CustomError(err.message,400);
     }
+
+    if(err.code === 11000) {
+        customError = new CustomError("Duplicated input error. Check your input field's :*",400);
+    }
     res.status(customError.status || 500).json({
         success:false,
         message:customError.message || "Internal server error."
